03796d9e726775bd0b1d1c877d12c70f51e4e569
g-otn <g_otn@hotmail.com>
2019-10-17 15:59:24 -0300

    add FutureBuilder for getData

--- conversor_moedas/lib/main.dart
+++ conversor_moedas/lib/main.dart
@@ -31,6 +31,35 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.amber,
       ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                  child: Text("Carregando dados",
+                      style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 25.0)
+                  ));
+            default:
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text("Erro ao Carregar Dados :(",
+                    style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 25.0
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                return Container(color: Color.fromRGBO(20, 20, 20, 1),);
+              }
+          }
+        },
+      ),
     );
   }
 }
