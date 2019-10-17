d80febc2fca29c0fb91b5d8ac445e532ba63c735
g-otn <g_otn@hotmail.com>
2019-10-17 09:42:32 -0300

    update: add main Text widgets

--- contador_pessoas/lib/main.dart
+++ contador_pessoas/lib/main.dart
@@ -2,7 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Contador de Pessoas",
-    home: Container(color: Colors.white,)
-  ));
+      title: "Contador de Pessoas",
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Pessoas: 0",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "Pode entrar!",
+            style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0),
+          )
+        ],
+      )));
 }
