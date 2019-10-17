d534c72defebc63bc84e96006fcaae1bf8a2c40d
g-otn <g_otn@hotmail.com>
2019-10-17 15:45:00 -0300

    create Scaffold

--- conversor_moedas/lib/main.dart
+++ conversor_moedas/lib/main.dart
@@ -8,11 +8,29 @@ const request = "https://api.hgbrasil.com/finance";
 
 void main() async {
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
   ));
 }
 
 Future<Map> getData() async {
   http.Response response = await http.get(request);
   return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Conversor de Moedas"),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+    );
+  }
 }
