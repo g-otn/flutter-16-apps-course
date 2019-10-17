5a6e24d6abb7648cd33c7c072c276526fa9b0608
g-otn <g_otn@hotmail.com>
2019-10-17 15:37:17 -0300

    refactor: API data gathering to function

--- conversor_moedas/lib/main.dart
+++ conversor_moedas/lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance";
 
 void main() async {
-
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)["results"]["currencies"]);
-
   runApp(MaterialApp(
     home: Container(),
   ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
