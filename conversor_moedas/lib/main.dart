c33bf326e432346a61897ec5745d1b22bd20d1ef
g-otn <g_otn@hotmail.com>
2019-10-17 15:31:17 -0300

    feat: get HG Finance API data

--- conversor_moedas/lib/main.dart
+++ conversor_moedas/lib/main.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
+
+const request = "https://api.hgbrasil.com/finance";
+
+void main() async {
+
+  http.Response response = await http.get(request);
+  print(json.decode(response.body)["results"]["currencies"]);
+
+  runApp(MaterialApp(
+    home: Container(),
+  ));
+}
