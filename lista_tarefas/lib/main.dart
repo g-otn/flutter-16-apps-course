dc8144aa3e13250c7cf2067b08955d37c86098ea
g-otn <g_otn@hotmail.com>
2019-10-18 09:59:34 -0300

    add file handling methods

--- lista_tarefas/lib/main.dart
+++ lista_tarefas/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:convert';
 
 void main() {
   runApp(MaterialApp(
@@ -12,6 +15,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List _toDoList = [];
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveDate() async {
+    String data = json.encode(_toDoList);
+    final File file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
