6167d065d60a4b80222fe612a260ba7f9aa89efd
g-otn <g_otn@hotmail.com>
2019-10-22 11:32:04 -0300

    fix snackbar event

--- lista_tarefas/lib/main.dart
+++ lista_tarefas/lib/main.dart
@@ -151,6 +151,7 @@ class _HomeState extends State<Home> {
             duration: Duration(seconds: 2),
           );
 
+          Scaffold.of(context).removeCurrentSnackBar();
           Scaffold.of(context).showSnackBar(snack);
         });
       },
